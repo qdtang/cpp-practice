a2822820e4c2c34322d9c89c4c5039d7a01f18cf
qdtang <qdtang.lance@gmail.com>
2018-09-15 22:34:56 +0800

    RAII for redir

--- redir/redir.cpp
+++ redir/redir.cpp
@@ -4,13 +4,43 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+class TempStdOut
+{
+	int _saved_stdout;
+public:
+	explicit TempStdOut(const char* path, const char* mode = "w") {
+		std::cout << std::flush;
+		_saved_stdout = dup(STDOUT_FILENO);
+		if (_saved_stdout < 0) {
+			perror("dup");
+		}
+		FILE* fp = fopen(path, mode);
+		if (!fp) {
+			perror("fopen");
+		}
+		int fd = fileno(fp);
+		if (dup2(fd, STDOUT_FILENO) < 0) {
+			perror("dup2 in ctor");
+		}
+		close(fd);
+	}
+	~TempStdOut() {
+		std::cout << std::flush;
+		if (dup2(_saved_stdout, STDOUT_FILENO) < 0) {
+			perror("dup2 in dtor");
+		}
+	}
+};
+
+void log(){
+	TempStdOut redirFile("log", "a");
+	std::cout << "Hello to file\n"; 
+}
+
 int main(int argc, char *argv[])
 {
-	int fd = open("log", O_WRONLY | O_CREAT);
-	if (dup2(fd, STDOUT_FILENO) < 0) {
-		perror("dup2");
-	}
-	std::cout << "Hello\n";
-	close(fd);
+	std::cout << "Hello to terminal\n";
+	log();
+	std::cout << "Hello to terminal\n";
 	return 0;
 }
