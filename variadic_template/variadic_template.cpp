01dd161ef779195825413077552fe160794c968b
qdtang <qdtang.lance@gmail.com>
2018-10-28 22:44:03 +0800

    polish print format

--- variadic_template/variadic_template.cpp
+++ variadic_template/variadic_template.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <sstream>
 
-// print1: old fashion variadic template version
+// print1: old fashion variadic template
 template <typename T>
 void print1(T&& t)
 {
@@ -10,25 +11,37 @@ void print1(T&& t)
 template <typename T, typename... Args>
 void print1(T&& t, Args... args)
 {
-	std::cout << std::forward<T>(t) << "\n";
+	std::cout << std::forward<T>(t) << " ";
 	print1(std::forward<Args>(args)...);
 }
 
-// print2: version using constexpr if
+// print2: using constexpr if
 template <typename T, typename... Args>
 void print2(T&& t, Args&&... args)
 {
-	std::cout << std::forward<T>(t) << "\n";
+	std::cout << std::forward<T>(t);
 	if constexpr (sizeof...(args)) {
+		std::cout << " ";
 		print2(std::forward<Args>(args)...);
+	}
+	else {
+		std::cout << "\n";
 	}
 }
 
-// print3: version using fold expression
+template <typename T>
+std::ostringstream writeToStream(T&& t)
+{
+	std::ostringstream oss;
+	oss << std::forward<T>(t) << " ";
+	return oss;
+}
+
+// print3: using fold expression
 template <typename... Args>
 void print3(Args... args)
 {
-	(std::cout << ... << args) << "\n";
+	(std::cout <<  ... << (writeToStream(args).str())) << "\n";
 }
 
 
