3dfb8309688757e8d20b576a286c393860ebf2c1
qdtang <qdtang.lance@gmail.com>
2018-11-21 23:23:16 +0800

    add filter for print_prime

---
+++ print_prime/filter.sh
@@ -0,0 +1,9 @@
+last=10
+if [ $# -gt 0 ]; then
+	last=$1
+fi
+
+output=$(make CXXFLAGS+=-DLAST=${last} |& grep prime_number)
+
+num=$(echo "${output}" | wc -l)
+echo "${output}" | head -n $((num/2))
