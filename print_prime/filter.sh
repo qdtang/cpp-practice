9965c6da1f3d6767f7a35ee6d43410b0313b7065
qdtang <qdtang.lance@gmail.com>
2018-11-22 16:07:31 +0800

    use sh instead of bash

--- print_prime/filter.sh
+++ print_prime/filter.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 last=10
 if [ $# -gt 0 ]; then
 	last=$1
 fi
 
-output=$(make CXXFLAGS+=-DLAST=${last} |& grep prime_number)
+output=$(make CXXFLAGS+=-DLAST=${last} 2>&1 | grep prime_number)
 
 num=$(echo "${output}" | wc -l)
 echo "${output}" | head -n $((num/2))
