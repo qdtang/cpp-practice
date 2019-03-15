640d54dea7d575062755c90c973387a94265f934
qdtang <qdtang.lance@gmail.com>
2019-03-15 16:47:09 +0800

    add visitor with deduction guide

--- visit_variant/visit_variant.cpp
+++ visit_variant/visit_variant.cpp
@@ -10,6 +10,14 @@ struct Visitor
    void operator()(const std::string& s) {std::cout << "string with value " << s << "\n";}
 };
 
+template <typename... Ts>
+struct overloaded : Ts...
+{
+	using Ts::operator()...;
+};
+// deduction guide (since C++17)
+template <typename... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
 int main()
 {
     using Var =  std::variant<int, double, std::string>;
@@ -27,11 +35,19 @@ int main()
         }
     };
 
+	auto ol = overloaded{
+		[](int i){std::cout << "int from overloaded: " << i << "\n";},
+		[](double d){std::cout << "double from overloaded: " << d << "\n";},
+		[](const std::string& s){std::cout << "string from overloaded: " << s << "\n";}
+	};
+
     std::visit(Visitor(), Var{"hello"});
     std::visit(Visitor(), Var{7});
     std::visit(Visitor(), Var{7.0});
     std::visit(visitor, Var{"world"});
     std::visit(visitor, Var{8});
     std::visit(visitor, Var{8.8});
+	std::visit(ol, Var{233});
+	std::visit(ol, Var{2.33});
     return 0;
 }
